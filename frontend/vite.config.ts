2daebadfefe548ea8773e0da08009f773abd4882
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2026-04-16 11:38:50 +0530

    refactor: update Vite configuration to include build options and improve chunking strategy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),tailwindcss()],
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Splits each node_module into its own chunk automatically
+          if (id.includes("node_modules")) {
+            const pkg = id.split("node_modules/")[1].split("/")[0];
+            return `vendor-${pkg}`;
+          }
+        },
+      },
+    },
+  },
+});
